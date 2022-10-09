31e8e7e26ee4d50990a05109e3b62aefb423c2b8
alexshilo <alexander.shilo@gmail.com>
2022-10-09 19:29:22 +0300

    20 задание выполнено

--- unit_one/task18.py
+++ unit_one/task18.py
@@ -10,7 +10,7 @@ import random
 #Выводы комбинаций оформить в список кортежей.
 
 #Вывод произвольных комбинаций
-numbers = list(range(1,7))
+numbers = list(range(1, 7))
 print (numbers)
 kost1 = random.randrange(1, len(numbers) + 1)
 kost2 = random.randrange(1, len(numbers) + 1)
@@ -21,6 +21,19 @@ summ = kost1 + kost2
 print("Сумма", summ, "  ", "комбинация [",comb,"]")
 
 #Вывод всех возможных комбинаций
+# def function_kosti():
+#   kosti = []
+#   for i in range(1,7):
+#       kost_1 = i + 1
+#     for j in range(1,7):
+#         kost_2 = j + 1
+#       kosti.append((i,j))
+#           summ = kost_1 + kost_2
+#   return kosti
+# print (summ)
+# print(function_kosti())
+
+
 
 
 

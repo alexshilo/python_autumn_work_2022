9355038711a72572ae084a638432507da5022f7b
alexshilo <alexander.shilo@gmail.com>
2022-10-11 09:27:52 +0300

    Задание 25 выполнено

---
+++ unit_one/task25.py
@@ -0,0 +1,16 @@
+#todo: Убрать повторяющиеся буквы и лишние символы
+# Построить по ключевой фразе часть алфавита. Взять все буквы по одному разу. Не буквы убрать.
+# Буквы должны идти в том порядке, в котором встретились во фразе в первый раз.
+
+# Input             	            Output
+#
+# apple	                        aple
+# 25.04.2022 Good morning !!	    godmrni
+
+x = "apple"
+y = "25.04.2022 Good morning !!"
+
+output_x = "".join(dict.fromkeys(x))
+output_y = "".join(dict.fromkeys(filter(str.isalpha, y.lower())))
+print (output_x)
+print (output_y)

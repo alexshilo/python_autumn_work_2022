58faddeeb62e8bb3d35f348e7f383bf40ccb8a19
alexshilo <alexander.shilo@gmail.com>
2022-10-15 19:58:27 +0300

    29 сделано

--- unit_one/task29.py
+++ unit_one/task29.py
@@ -9,14 +9,14 @@
 #
 # [[1, 4], [2, 5], [3, 6]]
 
-# Вариант_1
+#Вариант_1
 matrix = [[1, 2, 3], [4, 5, 6]]
 transpose_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-print(transpose_matrix)
+print("Решение без функции:", transpose_matrix)
 
-# Вариант_2
-#def transpose(matrix):
-#transpose(matrix)
-
-
-
+#Вариант_2
+def transpose(matrix = [[1, 2, 3], [4, 5, 6]]):
+    transpose_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    matrix = transpose_matrix
+    print("Решение с функцией:", matrix)
+transpose(matrix)

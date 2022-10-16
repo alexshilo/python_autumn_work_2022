f3bc21debcfca71a4a5a7624cfe652ef0c268911
alexshilo <alexander.shilo@gmail.com>
2022-10-16 22:05:31 +0300

    30 сделано!

--- unit_one/task30.py
+++ unit_one/task30.py
@@ -2,9 +2,18 @@
 # Написать функцию msum(matrix)  которая подсчитывает сумму всех элементов матрицы:
 # Задачу решить с помощью генераторов.
 
+#Вариант 1
 matrix = [[1, 2, 3], [4, 5, 6]]
 def msum(matrix):
     summ_of_elements = sum([sum(i) for i in matrix])
     print("Сумма элементов =", summ_of_elements)
 
 msum(matrix)
+
+#Вариант 2
+def msum(matrix):
+    summ_of_elements = sum([col for row in matrix for col in row])
+    print("Сумма элементов =", summ_of_elements)
+
+msum(matrix)
+

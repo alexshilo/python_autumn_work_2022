2bb3ee1ad2bc247422e5af50e8272a09661d43e3
alexshilo <alexander.shilo@gmail.com>
2022-11-07 15:45:48 +0300

    22

--- unit_one/task22.py
+++ unit_one/task22.py
@@ -4,33 +4,33 @@
 #  В этой задаче удобно считывать файл построчно, шифруя каждую строку в отдельности.
 # В каждой строчке содержатся различные символы. Шифровать нужно только буквы кириллицы.
 
-myfile = open("message.txt", "r", encoding="utf-8")
+myfile = open("message.txt", mode = "r", encoding = "utf-8")
 
 cyr_low = set("съешь же еще этих мягких французских булок да выпей чаю".replace(" ", ""))
 cyr_high = [x.upper() for x in cyr_low]
-sorted_cyr_low, sorted_cyr_high = sorted(cyr_low), sorted(cyr_high)
-sorted_cyr = sorted_cyr_low + sorted_cyr_high
-print(sorted_cyr)
+sorted_cyr_low, sorted_cyr_high = ''.join(sorted(cyr_low)), ''.join(sorted(cyr_high))
+print(sorted_cyr_low)
+print(sorted_cyr_high)
 
-shift = 0
+
 count = len(myfile.readlines())
 myfile.seek(0,0)
 encryption = ''
-while shift <= count:
+shift = 0
+
+while shift <= count:                          #поправить содержимое цикла
     shift += 1
     line = myfile.readlines()
-    for i in line:
-        if i.isupper():
-
-            ######## в работе"
-            # i_unicode = ord(i)
-            # i_index = ord(i) - ord("А")
-            # new_index = (i_index + shift)%32
-            # new_unicode = new_index + ord("А")
-            # new_character = chr(new_unicode)
-            # encryption = encryption + new_character
+    for letter in line:
+        x = letter.lower()
+        position = sorted_cyr_low.find(x)
+        new_position = position - shift
+        if letter in sorted_cyr_low:
+            encryption = encryption + sorted_cyr_low[new_position]
+        elif letter in sorted_cyr_high:
+            encryption = encryption + sorted_cyr_high[new_position]
         else:
-            encryption += i
+            encryption = encryption + letter
 
 print(encryption)
 
